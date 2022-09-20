91fe36e3a07583e51c17b5b99a27a886b22986b3
OLLO77 <petkooleg@gmail.com>
2022-09-20 12:58:35 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styles from './App.module.css'
+import {useState} from "react";
+import {CgArrowLongDownL, CgArrowLongUpL } from "react-icons/cg";
 function App() {
+
+  const statuses = ['todo', 'progress', 'review', 'done']
+  const [task, setTask] = useState([])
+  const [newTask, setNewTask] =useState('')
+    const addTask = () => {
+      setTask([...task, {id: Math.random(), title: newTask, status: statuses[0]}])
+        setNewTask('')
+    }
+    const deleteTask = (idTask) => {
+      setTask(task.filter(task => task.id !== idTask))
+    }
+    const moveTask = (idTask) => {
+      setTask(task.map((task, index) => task.id === idTask ? {...task, status: statuses[task.index] - 1} : task))
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <input className={styles.Input} placeholder='add new task' value={newTask} onChange={e => setNewTask(e.target.value)}/>
+      <button className={styles.Button} onClick={addTask}>ADD TASK</button>
+      {statuses.map(status =>
+          <div>
+            <h2 className={styles.H2}>
+              {status}
+            </h2>
+              {task.filter(task => task.status === status).map((task, index) =>
+              <h5 className={styles.H5}>
+                <button className={styles.Button} onClick={() => moveTask(task.id, index)}><CgArrowLongDownL /></button>
+                  {task.title}
+                <button className={styles.Button}><CgArrowLongUpL /></button>
+                <button className={styles.Button} onClick={() => deleteTask(task.id)}>DELETE</button>
+              </h5>)}
+          </div>
+      )}
+
     </div>
   );
 }
